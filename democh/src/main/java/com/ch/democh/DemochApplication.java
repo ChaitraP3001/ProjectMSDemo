f815ab0c7225a33633cc635980d8f6c1b28fedb4
student <chaitrapg6@gmail.com>
2023-09-10 10:40:45 +0000

    First commit

---
+++ democh/src/main/java/com/ch/democh/DemochApplication.java
@@ -0,0 +1,13 @@
+package com.ch.democh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemochApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemochApplication.class, args);
+	}
+
+}
